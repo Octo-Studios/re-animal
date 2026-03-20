db31b70220db579efe3589cf295760799162b225
sunbatheproductions <retiredjjj@gmail.com>
2026-03-20 14:14:27 -0400

    Create RedPandaAI.java

---
+++ src/main/java/it/hurts/shatterbyte/reanimal/common/entity/red_panda/RedPandaAI.java
@@ -0,0 +1,128 @@
+package it.hurts.shatterbyte.reanimal.common.entity.redpanda;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.mojang.datafixers.util.Pair;
+import it.hurts.shatterbyte.reanimal.init.ReAnimalEntities;
+import it.hurts.shatterbyte.reanimal.init.ReAnimalSensorTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.ai.behavior.*;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.memory.MemoryStatus;
+import net.minecraft.world.entity.ai.sensing.Sensor;
+import net.minecraft.world.entity.ai.sensing.SensorType;
+import net.minecraft.world.entity.schedule.Activity;
+
+import java.util.Set;
+
+public class RedPandaAI {
+    private static final ImmutableList<SensorType<? extends Sensor<? super RedPandaEntity>>> SENSOR_TYPES = ImmutableList.of(
+            SensorType.NEAREST_LIVING_ENTITIES,
+            SensorType.NEAREST_PLAYERS,
+            SensorType.HURT_BY,
+            SensorType.NEAREST_ADULT
+    );
+
+    private static final ImmutableList<MemoryModuleType<?>> MEMORY_TYPES = ImmutableList.of(
+            MemoryModuleType.IS_PANICKING,
+            MemoryModuleType.HURT_BY,
+            MemoryModuleType.HURT_BY_ENTITY,
+            MemoryModuleType.WALK_TARGET,
+            MemoryModuleType.LOOK_TARGET,
+            MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE,
+            MemoryModuleType.PATH,
+            MemoryModuleType.NEAREST_VISIBLE_LIVING_ENTITIES,
+            MemoryModuleType.TEMPTING_PLAYER,
+            MemoryModuleType.TEMPTATION_COOLDOWN_TICKS,
+            MemoryModuleType.GAZE_COOLDOWN_TICKS,
+            MemoryModuleType.IS_TEMPTED,
+            MemoryModuleType.BREED_TARGET,
+            MemoryModuleType.NEAREST_VISIBLE_ADULT
+    );
+
+    public static Brain.Provider<RedPandaEntity> brainProvider() {
+        return Brain.provider(MEMORY_TYPES, SENSOR_TYPES);
+    }
+
+    public static Brain<?> makeBrain(Brain<RedPandaEntity> brain) {
+        initCoreActivity(brain);
+        initIdleActivity(brain);
+        initRestActivity(brain);
+        initPanicActivity(brain);
+
+        brain.setCoreActivities(Set.of(Activity.CORE));
+        brain.setDefaultActivity(Activity.IDLE);
+        brain.useDefaultActivity();
+
+        return brain;
+    }
+
+    private static void initCoreActivity(Brain<RedPandaEntity> brain) {
+        brain.addActivity(
+                Activity.CORE,
+                0,
+                ImmutableList.of(
+                        new Swim(0.8F),
+                        new LookAtTargetSink(45, 90),
+                        new MoveToTargetSink(),
+                        new CountDownCooldownTicks(MemoryModuleType.TEMPTATION_COOLDOWN_TICKS),
+                        new CountDownCooldownTicks(MemoryModuleType.GAZE_COOLDOWN_TICKS)
+                )
+        );
+    }
+
+    private static void initIdleActivity(Brain<RedPandaEntity> brain) {
+        brain.addActivity(
+                Activity.IDLE,
+                ImmutableList.of(
+                        Pair.of(0, SetEntityLookTargetSometimes.create(EntityType.PLAYER, 6F, UniformInt.of(30, 60))),
+                        Pair.of(1, new AnimalMakeLove(ReAnimalEntities.RED_PANDA.get(), 1.0F, 1)),
+                        Pair.of(2, new RunOne<>(
+                                ImmutableList.of(
+                                        Pair.of(new FollowTemptation(entity -> 1.25F, entity -> entity.isBaby() ? 1D : 2D), 1),
+                                        Pair.of(BabyFollowAdult.create(UniformInt.of(2, 4), 1.25F), 1)
+                                )
+                        )),
+                        Pair.of(3, new RandomLookAround(UniformInt.of(150, 250), 30F, 0F, 0F)),
+                        Pair.of(4, new RunOne<>(
+                                ImmutableMap.of(MemoryModuleType.WALK_TARGET, MemoryStatus.VALUE_ABSENT),
+                                ImmutableList.of(
+                                        Pair.of(RandomStroll.stroll(1F), 2),
+                                        Pair.of(SetWalkTargetFromLookTarget.create(1F, 3), 2),
+                                        Pair.of(new DoNothing(30, 60), 1)
+                                )
+                        ))
+                )
+        );
+    }
+
+    private static void initRestActivity(Brain<RedPandaEntity> brain) {
+        brain.addActivity(
+                Activity.REST,
+                ImmutableList.of(
+                        Pair.of(0, new DoNothing(30, 60))
+                )
+        );
+    }
+
+    private static void initPanicActivity(Brain<RedPandaEntity> brain) {
+        brain.addActivityAndRemoveMemoryWhenStopped(
+                Activity.PANIC,
+                10,
+                ImmutableList.of(new AnimalPanic<>(1.5F, mob -> DamageTypeTags.PANIC_CAUSES)),
+                MemoryModuleType.IS_PANICKING
+        );
+    }
+
+    public static void updateActivity(RedPandaEntity entity) {
+        if (entity.level().isDay() && !entity.getBrain().hasMemoryValue(MemoryModuleType.IS_PANICKING)) {
+            entity.getBrain().setActiveActivityToFirstValid(ImmutableList.of(Activity.PANIC, Activity.REST, Activity.IDLE));
+        } else {
+            entity.getBrain().setActiveActivityToFirstValid(ImmutableList.of(Activity.PANIC, Activity.IDLE));
+        }
+    }
+}
